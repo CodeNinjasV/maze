96d987047b6315543be2d778aef4d17e8b4c627a
CodeNinjasV <76259543+CodeNinjasV@users.noreply.github.com>
2020-12-19 11:49:59 -0500

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`1000100002010102020202020202020202020101020201020202020202020202020101010202010102020202020202020201020101020201020202010101010101010201010202010202020102020201010202010102020102020201020101010101010101020101020201020201030303030303010201010202010202010101010101010102020101010102020201010303030301020202010102020202010101010101010101010101010101010101030303030202010103030101010101010303030302020101030301010303010103030303020201010303010103030101010101010101010103030101030303030303030104010101030301010101010101030305`, img`
+2 . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+2 2 . 2 2 2 2 2 2 2 2 2 2 . . . 
+2 2 . . 2 2 2 2 2 2 2 2 2 . 2 . 
+. 2 2 . 2 2 2 . . . . . . . 2 . 
+. 2 2 . 2 2 2 . 2 2 2 . . 2 2 . 
+. 2 2 . 2 2 2 . 2 . . . . . . . 
+. 2 . . 2 2 . 2 2 . 2 2 2 2 2 2 
+. 2 . . 2 2 . 2 2 . . . . . . . 
+. 2 2 . . . . 2 2 2 . . 2 2 2 2 
+. 2 2 2 . . 2 2 2 2 . . . . . . 
+. . . . . . . . . . . . 2 2 2 2 
+2 2 . . 2 2 . . . . . . 2 2 2 2 
+2 2 . . 2 2 . . 2 2 . . 2 2 2 2 
+2 2 . . 2 2 . . 2 2 . . . . . . 
+. . . . 2 2 . . 2 2 2 2 2 2 2 . 
+. . . . 2 2 . . . . . . . 2 2 . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.collectibleInsignia,sprites.dungeon.stairLarge], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
